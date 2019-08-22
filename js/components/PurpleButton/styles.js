0a18a838093e09b96a1b1cb54919a7d42297f427
Oli Wright <oli.wright@blueyonder.co.uk>
2019-08-22 14:06:37 -0700

    fixed styling on purple button

--- js/components/PurpleButton/styles.js
+++ js/components/PurpleButton/styles.js
@@ -4,24 +4,23 @@ import theme from '../../config/theme';
 const styles = StyleSheet.create({
   button: {
     borderRadius: 35,
-    paddingLeft: 15,
-    paddingRight: 15,
-    paddingTop: 15,
-    paddingBottom: 15,
     margin: 30,
     overflow: 'hidden',
-    width: 'auto',
+    display: 'flex',
   },
   buttonText: {
     color: 'white',
-    fontSize: 20,
+    fontSize: 18,
     fontFamily: theme.fonts.regular,
     textAlign: 'center',
+    padding: 20,
   },
   gradient: {
     position: 'absolute',
     width: '100%',
-    height: '400%',
+    height: '100%',
+    padding: 0,
+    margin: 0,
   },
 });
 
